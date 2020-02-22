d59ba06e5e51642559fd9a2c18902e0fdd226ef7
Feilong <feilong.dragona@gmail.com>
2020-02-22 19:50:16 +0800

    usable app

--- app/src/main/java/mg/studio/android/guessme/MainActivity.java
+++ app/src/main/java/mg/studio/android/guessme/MainActivity.java
@@ -14,6 +14,7 @@ package mg.studio.android.guessme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -21,16 +22,17 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
-
 public class MainActivity extends AppCompatActivity {
     Button button;
-    EditText et_input;
+    EditText editText;
     int random_guess;
     TextView display;
+    ImageView imgDown, imgUp, imgMood;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +41,22 @@ public class MainActivity extends AppCompatActivity {
 
         random_guess = generatorGuess();
 
+        // Connect the images
+        imgDown = findViewById(R.id.img_arrow_down);
+        imgUp = findViewById(R.id.img_arrow_up);
+        imgMood = findViewById(R.id.img_mood);
+
+
+        imgUp.setVisibility(View.INVISIBLE);
+        imgDown.setVisibility(View.INVISIBLE);
+        imgMood.setVisibility(View.INVISIBLE);
+
         // constantly check the user input, if [1-1000], set
         // button visible
         button = findViewById(R.id.btn_check);
         display = findViewById(R.id.tv_info);
-        et_input = findViewById(R.id.et_input);
-        et_input.addTextChangedListener(new TextWatcher() {
+        editText = findViewById(R.id.et_input);
+        editText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 Log.e("TAG", "beforeTextChanged");
@@ -53,17 +65,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 Log.e("TAG", "onTextChanged");
+                imgVisibility(2);
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
-                // ensure user input is [1 - 1000]
-                String input = et_input.getText().toString();
-                Log.e("TAG", input);
+                // ensure that the user input is [1 - 1000]
+                String input = editText.getText().toString();
+                if (input.isEmpty()) {
+                    imgVisibility(2); // images set invisible
+                }
                 if (!input.isEmpty() && Integer.valueOf(input) > 0 && Integer.valueOf(input) <= 1000) {
                     // Set the button visible
                     button.setVisibility(View.VISIBLE);
-                    display.setVisibility(View.GONE);
+                    display.setVisibility(View.INVISIBLE);
                 } else {
                     button.setVisibility(View.INVISIBLE);
                     // inform the user about the valid inputs
@@ -92,13 +107,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btnCheck(View view) {
-        int user_input = Integer.valueOf(et_input.getText().toString());
+        int user_input = Integer.valueOf(editText.getText().toString());
         if (compare(user_input) > 0) {
-            Toast.makeText(this, "The hidden value is greater than " + user_input, Toast.LENGTH_LONG).show();
+            imgVisibility(-1);
         } else if (compare(user_input) < 0) {
-            Toast.makeText(this, "The hidden value is smaller than " + user_input, Toast.LENGTH_LONG).show();
+            imgVisibility(1);
         } else {
-            Toast.makeText(this, "You got it ! \n" + user_input, Toast.LENGTH_LONG).show();
+            imgVisibility(0);
         }
     }
 
@@ -112,5 +127,36 @@ public class MainActivity extends AppCompatActivity {
     private int generatorGuess() {
         //todo : implement the real generator
         return 345;
+    }
+
+    private void imgVisibility(int value) {
+        if (value == -1) {
+            // value entered too low
+            imgDown.setBackgroundResource(R.drawable.down_grey); // down colored
+            imgUp.setBackgroundResource(R.drawable.up_color); // down colored
+            imgMood.setBackgroundResource(R.drawable.fail); // mood fail
+            imgDown.setVisibility(View.VISIBLE);
+            imgUp.setVisibility(View.VISIBLE);
+            imgMood.setVisibility(View.VISIBLE);
+        } else if (value == 1) {
+            // value entered too high
+            imgDown.setBackgroundResource(R.drawable.down_color);
+            imgUp.setBackgroundResource(R.drawable.up_grey);
+            imgMood.setBackgroundResource(R.drawable.fail);
+            imgDown.setVisibility(View.VISIBLE);
+            imgUp.setVisibility(View.VISIBLE);
+            imgMood.setVisibility(View.VISIBLE);
+        } else if (value == 0) {
+            // Win state
+            imgDown.setVisibility(View.INVISIBLE);
+            imgUp.setVisibility(View.INVISIBLE);
+            imgMood.setBackgroundResource(R.drawable.win);
+            imgMood.setVisibility(View.VISIBLE);
+        } else {
+            imgDown.setVisibility(View.INVISIBLE);
+            imgUp.setVisibility(View.INVISIBLE);
+            imgMood.setBackgroundResource(R.drawable.fail);
+            imgMood.setVisibility(View.INVISIBLE);
+        }
     }
 }
